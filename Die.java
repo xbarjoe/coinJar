0258e0e06d6c23dae447c7cd7da5c2a89a8ebb5d
xbarjoe <stephen.owen1@richmond.edu>
2020-01-20 12:33:09 -0500

    added gui winow and die

---
+++ Die.java
@@ -0,0 +1,11 @@
+package coinJar;
+
+public class Die {
+	private double probSix;
+	public Die() {
+		probSix=1.0;
+	}
+	public Die(int diff) {
+		
+	}
+}
---
+++ Game.java
@@ -0,0 +1,45 @@
+package coinJar;
+import javax.swing.*;
+import java.awt.*;
+public class Game {
+	public static void setup() {
+		 //Creating the Frame
+        JFrame frame = new JFrame("coinJar");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 400);
+
+        //Creating the MenuBar and adding components
+        JMenuBar mb = new JMenuBar();
+        JMenu m1 = new JMenu("FILE");
+        JMenu m2 = new JMenu("Help");
+        mb.add(m1);
+        mb.add(m2);
+        JMenuItem m11 = new JMenuItem("New Game");
+        JMenuItem m22 = new JMenuItem("Exit");
+        m1.add(m11);
+        m1.add(m22);
+
+        //Creating the panel at bottom and adding components
+        JPanel panel = new JPanel(); // the panel is not visible in output
+        JButton roll = new JButton("Roll");
+        JButton pass = new JButton("Pass");
+        panel.add(roll);
+        panel.add(pass);
+
+        // Text Area at the Center
+        JTextArea ta = new JTextArea();
+
+        //Adding Components to the frame.
+        frame.getContentPane().add(BorderLayout.SOUTH, panel);
+        frame.getContentPane().add(BorderLayout.NORTH, mb);
+        frame.getContentPane().add(BorderLayout.CENTER, ta);
+        frame.setVisible(true);
+	}
+	public static void main(String args[]){
+		setup();
+		String[] options = {"Easy", "Normal", "Hard"};
+		int x = JOptionPane.showOptionDialog(null, "Choose your difficulty...", "Click a button",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[1]);
+        Die d = new Die(x);
+  }
+}
